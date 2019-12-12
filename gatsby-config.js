1d29a32765d40047a55198d4bcf2419fa09da6b2
nazeh <ar.nazeh@gmail.com>
2019-12-12 10:29:46 +0300

    Config :books: update siteMetada and package.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
-    description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@gatsbyjs',
+    title: 'Nazeh | Full Stack Developer',
+    description: 'Self Taught Full Stack Developer.',
+    author: 'Nazeh',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
