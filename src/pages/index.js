7eb14d7845ce8f7a19b4b4dc7074e7a69478dba8
nazeh <ar.nazeh@gmail.com>
2020-01-03 10:52:45 +0300

    :construction: Start contacts section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from '../layout';
 import Projects from '../containers/projects';
+import Contact from '../containers/contact';
 import SEO from '../utils/seo';
 import './main.css';
 import 'typeface-montserrat';
@@ -9,6 +10,7 @@ const IndexPage = () => (
   <Layout>
     <SEO />
     <Projects />
+    <Contact />
   </Layout>
 );
 
